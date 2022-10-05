015791331bb47e9fcf3dda58b528b2f2eb6760bf
cerobinsonl <60712960+cerobinsonl@users.noreply.github.com>
2022-10-05 13:03:40 -0500

    aber 4

--- server/main.js
+++ server/main.js
@@ -5,15 +5,15 @@ import "../imports/api/orders";
 
 Meteor.startup(() => {
   // code to run on server at startup
-    Roles.addUsersToRoles("vYDX7rXKLAGz3goQ5", 'minorista');
-    Roles.addUsersToRoles("EXS6xFMfYgBJr2uhw", 'minorista');
-    Roles.addUsersToRoles("wffmGJDABgWhDboWT", 'minorista');
-    Roles.addUsersToRoles("PqL5L8egzgvwGbQ2E", 'minorista');
+    Roles.setUserRoles("vYDX7rXKLAGz3goQ5", 'minorista');
+    Roles.setUserRoles("EXS6xFMfYgBJr2uhw", 'minorista');
+    Roles.setUserRoles("wffmGJDABgWhDboWT", 'minorista');
+    Roles.setUserRoles("PqL5L8egzgvwGbQ2E", 'minorista');
 
-    Roles.addUsersToRoles("ChZWr6iwZLS4aeByn", 'planta');
+    Roles.setUserRoles("ChZWr6iwZLS4aeByn", 'planta');
 
-    Roles.addUsersToRoles("9Fxm5cRTnLc5vm3YD", 'cd');
+    Roles.setUserRoles("9Fxm5cRTnLc5vm3YD", 'cd');
 
-    Roles.addUsersToRoles("otX2zZL9HiH7nPap9", 'admin');
+    Roles.setUserRoles("otX2zZL9HiH7nPap9", 'admin');
 
 });
