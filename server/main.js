f42d144f2d0a057648694df487b6c07572f24c4a
cerobinsonl <60712960+cerobinsonl@users.noreply.github.com>
2023-03-29 22:17:28 -0500

    Puntaje2

--- server/main.js
+++ server/main.js
@@ -5,18 +5,6 @@ import "../imports/api/orders";
 
 Meteor.startup(() => {
   // code to run on server at startup
-<<<<<<< HEAD
-    Roles.setUserRoles("vYDX7rXKLAGz3goQ5", 'minorista');
-    Roles.setUserRoles("EXS6xFMfYgBJr2uhw", 'minorista');
-    Roles.setUserRoles("wffmGJDABgWhDboWT", 'minorista');
-    Roles.setUserRoles("PqL5L8egzgvwGbQ2E", 'minorista');
-
-    Roles.setUserRoles("ChZWr6iwZLS4aeByn", 'planta');
-
-    Roles.setUserRoles("9Fxm5cRTnLc5vm3YD", 'cd');
-
-    Roles.setUserRoles("otX2zZL9HiH7nPap9", 'admin');
-=======
     Roles.setUserRoles("nA3mpTyp82cvoB5H9", 'minorista');
     Roles.setUserRoles("B6kAnGu9ENkkXzWGH", 'minorista');
     Roles.setUserRoles("LW3rg7druqbRhiRJg", 'minorista');
@@ -27,6 +15,5 @@ Meteor.startup(() => {
     Roles.setUserRoles("TwcxoPqK3sAjfrHkk", 'cd');
 
     Roles.setUserRoles("vhC8zvthpMR33joGf", 'admin');
->>>>>>> 26cbfdd615cc98c27ae3fad4df01c21958e31f91
 
 });
